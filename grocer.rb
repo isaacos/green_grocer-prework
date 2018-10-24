4ae1d43029964c560c67b590742864f667f43291
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-24 18:19:40 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,32 @@
-def consolidate_cart(cart)
-  # code here
+def consolidate_cart(original_cart)
+
+consolidated_hash = {}
+
+  original_cart.each do |hashes|
+
+    hashes.each do |food_name, details|
+
+      if !consolidated_hash.include?(food_name)
+
+      consolidated_hash[food_name] = details
+      consolidated_hash[food_name][:count] = 1
+
+      elsif consolidated_hash.include?(food_name)
+      consolidated_hash[food_name][:count] +=1
+
+      end
+
+    end
+
+  end
+consolidated_hash
+
 end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+ def apply_coupons(consolidated_hash, coupon)
+
+return coupon.keys
+ end
 
 def apply_clearance(cart)
   # code here
